b79f13c785de1635ed0a354fb8a6c222ada690f3
Pankaj Aditya <paditya@firstam.com>
2020-01-25 17:18:50 +0530

    km commit1

---
+++ evenodd1.py
@@ -0,0 +1,8 @@
+i=[1,2,3,4,5,10]
+
+for a in i:
+    
+    if a % 2 == 0:
+        print(a, "is even")
+    else:
+        print(a,"is odd")
---
+++ km.py
@@ -0,0 +1,7 @@
+km=9
+def mile():
+
+    m=3.6*km
+    return m
+    print("m is: ")
+print(mile())
